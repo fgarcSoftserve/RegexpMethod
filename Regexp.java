323689ddce9f5edf0a798bd3f03b266c6fe72c15
Fernando García Barajas <fgarciab@FerMacAir.local>
2023-09-10 14:46:42 -0600

    UC#10 Add validations

--- Regexp.java
+++ Regexp.java
@@ -3,23 +3,41 @@ import java.util.regex.Pattern;
 
 public class Regexp {
 
-    public static void main(String[] args){
-        System.out.println("Match: "+Regexp.matchText(args[0], args[1]));
+    public static void main(String[] args) {
+        boolean match = true;
+        if (args.length < 2) {
+            match = false;
+        } else {
+            try {
+                Integer.parseInt(args[1]);
+            } catch (NumberFormatException nfe) {
+                match = false;
+            }
+        }
+
+        if (match) {
+            match = Regexp.matchText(args[0], args[1]);
+        }
+        Regexp.printResult(match);
+    }
+
+    private static void printResult(boolean match) {
+        System.out.println("Match: " + match);
     }
 
 
-    public static boolean matchText(String text, String maxLength){
+    public static boolean matchText(String text, String maxLength) {
         String regexp = "^(?!.\"\\\\S+\")" +
                 "(?=.*[a-z])(?=.*[A-Z])" +
                 "(?=.*\\d)(?=.*[\\\\{}!\"#$%&'()*+,-./:;<=>?@^_`|~\\[\\]])" +
                 "[A-Za-z\\d\\\\{}!\"#$%&'()*+,-./:;<=>?@^_`|~\\[\\]]" +
-                "{1,"+maxLength+"}$";
+                "{1," + maxLength + "}$";
         final Pattern pattern = Pattern.compile(regexp);
         final Matcher matcher = pattern.matcher(text);
-        if(matcher.find()){
+        if (matcher.find()) {
             return true;
         }
-        return  false;
+        return false;
     }
 
 
